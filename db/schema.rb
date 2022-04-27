f32123d373a1ed1f6f2bd67b8ef2ea25e2eb6a77
codescaptain <ahmet-57-@hotmail.com>
2022-04-28 01:18:15 +0300

    [add] slug column for article

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_26_231525) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_04_27_221454) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_26_231525) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug", null: false
   end
 
 end
