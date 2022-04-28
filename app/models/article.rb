f489edcac294053ebca5e237a197b4db0d004655
codescaptain <ahmet-57-@hotmail.com>
2022-04-28 03:21:05 +0300

    [add] FriendlyId gem

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  validates :title, presence:true, length: { minimum: 5 }
+  validates :content, presence:true, length: { minimum: 10 }
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
 end
--- db/migrate/20220427221454_add_slug_to_article.rb
+++
@@ -1,5 +0,0 @@
-class AddSlugToArticle < ActiveRecord::Migration[7.0]
-  def change
-    add_column :articles, :slug, :string, unique: true, null: false
-  end
-end
